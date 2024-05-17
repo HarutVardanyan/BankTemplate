1a0927066b2f8cd59aa7190dab56d21c12eb2203
MyFirstLesson <macbookpro@MacBooks-MBP.wind3.hub>
2024-05-17 15:24:36 +0200

    initial commit

---
+++ BankTemplate/BankTemplate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BankTemplate
+//
+//  Created by MacBook Pro on 5/17/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
