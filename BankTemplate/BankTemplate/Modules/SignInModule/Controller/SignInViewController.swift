fa2cebf171dc723e41bfb967478e142caa634315
IOSHar <macbookpro@MacBooks-MBP.wind3.hub>
2024-05-23 14:05:13 +0200

    add 2 screen

--- BankTemplate/BankTemplate/Modules/SignInModule/Controller/SignInViewController.swift
+++ BankTemplate/BankTemplate/Modules/SignInModule/Controller/SignInViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SignInViewController.swift
 //  BankTemplate
 //
 //  Created by MacBook Pro on 5/17/24.
@@ -26,6 +26,14 @@ final class SignInViewController: UIViewController {
             static let trailingSignInButton: CGFloat = -103
             static let leadingSignInButton: CGFloat = 102
             static let heightSignInButton: CGFloat = 48
+            static let topSignUpButton: CGFloat = 443
+            static let leadingSignUpButton: CGFloat = 136
+            static let trailingSignUpButton: CGFloat = -138
+            static let bottomSignUpButton: CGFloat = -383
+            static let topDontHaveLabel: CGFloat = 409
+            static let leadingDontHaveLabel: CGFloat = 74
+            static let trailingDontHaveLabel: CGFloat = -75
+            static let bottomDontHaveLabel: CGFloat = -409
         }
         enum Texts {
             static let bankLogo = "bankLogo"
@@ -33,6 +41,8 @@ final class SignInViewController: UIViewController {
             static let textColor = "textColor"
             static let emailText = "Email"
             static let emailPassword = "Password"
+            static let signUpButtonColor = "signUpButtonColor"
+            static let iDontHaveAccaount = "Don't have an account yet?"
             }
     }
     
@@ -84,6 +94,30 @@ final class SignInViewController: UIViewController {
         return logButton
     }()
     
+    private let goSignUpButton: UIButton = {
+        let signUpButton = UIButton()
+        
+        signUpButton.setTitle("Sign Up", for: .normal)
+        signUpButton.setTitleColor(UIColor(
+            named: Constants.Texts.signUpButtonColor),
+            for: .normal)
+        signUpButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
+        signUpButton.addTarget(self, action: #selector(signUpButtonTapped), for: .touchUpInside)
+        
+        return signUpButton
+    }()
+    
+    private let dontHaveLabel = {
+        let label = UILabel()
+        
+        label.text = Constants.Texts.iDontHaveAccaount
+        label.font = .systemFont(ofSize: 15)
+        label.textColor = .black
+        label.textAlignment = .center
+        
+        return label
+    }()
+    
     //MARK: - Life Cycle
     
     override func viewDidLoad() {
@@ -102,7 +136,15 @@ final class SignInViewController: UIViewController {
         view.addSubview(emailTextField)
         view.addSubview(passwordlTextField)
         view.addSubview(signInButton)
+        view.addSubview(goSignUpButton)
+        view.addSubview(dontHaveLabel)
 
+    }
+    
+    @objc private func signUpButtonTapped() {
+        let signUpViewController = SignUpViewController()
+        signUpViewController.modalPresentationStyle = .fullScreen
+        present(signUpViewController, animated: true, completion: nil)
     }
     
     private func configureConstraints() {
@@ -110,10 +152,15 @@ final class SignInViewController: UIViewController {
         emailTextField.translatesAutoresizingMaskIntoConstraints = false
         passwordlTextField.translatesAutoresizingMaskIntoConstraints = false
         signInButton.translatesAutoresizingMaskIntoConstraints = false
+        goSignUpButton.translatesAutoresizingMaskIntoConstraints = false
+        dontHaveLabel.translatesAutoresizingMaskIntoConstraints = false
+        
         configureSignInLogoConstraints()
         configureTextFieldEmailConstraints()
         configureTextFieldPassConstraints()
         configureSignInButtonConstraints()
+        configureGoSignUpButtonConstraints()
+        configureDontHaveLabelConstraints()
     }
 }
 
@@ -191,6 +238,46 @@ extension SignInViewController{
         ),
             signInButton.heightAnchor.constraint(
                 equalToConstant: Constants.Insets.heightSignInButton
+        )
+     ])
+   }
+    private func configureGoSignUpButtonConstraints() {
+        NSLayoutConstraint.activate([
+            goSignUpButton.topAnchor.constraint(
+                equalTo: view.topAnchor,
+                constant: Constants.Insets.topSignUpButton
+        ),
+            goSignUpButton.leadingAnchor.constraint(
+                equalTo: view.leadingAnchor,
+                constant: Constants.Insets.leadingSignUpButton
+        ),
+            goSignUpButton.trailingAnchor.constraint(
+                equalTo: view.trailingAnchor,
+                constant: Constants.Insets.trailingSignUpButton
+        ),
+            goSignUpButton.bottomAnchor.constraint(
+                equalTo: view.bottomAnchor,
+                constant: Constants.Insets.bottomSignUpButton
+        )
+     ])
+   }
+    private func configureDontHaveLabelConstraints() {
+        NSLayoutConstraint.activate([
+            dontHaveLabel.topAnchor.constraint(
+                equalTo: view.topAnchor,
+                constant: Constants.Insets.topDontHaveLabel
+        ),
+            dontHaveLabel.leadingAnchor.constraint(
+                equalTo: view.leadingAnchor,
+                constant: Constants.Insets.leadingDontHaveLabel
+        ),
+            dontHaveLabel.trailingAnchor.constraint(
+                equalTo: view.trailingAnchor,
+                constant: Constants.Insets.trailingDontHaveLabel
+        ),
+            dontHaveLabel.bottomAnchor.constraint(
+                equalTo: view.bottomAnchor,
+                constant: Constants.Insets.bottomDontHaveLabel
         )
      ])
    }
---
+++ BankTemplate/BankTemplate/Modules/SignUpModule/Controller/SignUpViewController.swift
@@ -0,0 +1,284 @@
+//
+//  SignUpViewController.swift
+//  BankTemplate
+//
+//  Created by MacBook Pro on 5/23/24.
+//
+import UIKit
+
+/// Ekran Dlya Registracii
+
+final class SignUpViewController: UIViewController {
+    
+    // MARK: - Constants
+    
+    enum Constants {
+        enum Insets {
+            static let topSignUpLogo: CGFloat = 143
+            static let bottomtextFieldEmail: CGFloat = 141
+            static let leadingSignUpLogo: CGFloat = 39
+            static let trailingSignUpLogo: CGFloat = -40
+            static let heightSignUpLogo: CGFloat = 243
+            static let heightTextFieldEmail: CGFloat = 50
+            static let bottomTextFieldPass: CGFloat = 30
+            static let topSignUpButton: CGFloat = 58
+            static let trailingSignUpButton: CGFloat = -103
+            static let leadingSignUpButton: CGFloat = 102
+            static let heightSignUpButton: CGFloat = 48
+            static let topSignInButton: CGFloat = 443
+            static let leadingSignInButton: CGFloat = 136
+            static let trailingSignInButton: CGFloat = -138
+            static let bottomSignInButton: CGFloat = -383
+            static let topHaveLabel: CGFloat = 409
+            static let leadingHaveLabel: CGFloat = 74
+            static let trailingHaveLabel: CGFloat = -75
+            static let bottomHaveLabel: CGFloat = -409
+        }
+        enum Texts {
+            static let signUpPhoto = "signUpImage"
+            static let textEmailBackColor = "textFildColor"
+            static let textColor = "textFildTextColor"
+            static let emailText = "Email"
+            static let emailPassword = "Password"
+            static let signInButtonColor = "signUpButtonColor"
+            static let iHaveAccaount = "Already have an account?"
+            }
+    }
+    
+    //MARK: - Visual Components
+    
+    private let signUpLogo = {
+        let signImage = UIImageView()
+        
+        signImage.image = UIImage(named: Constants.Texts.signUpPhoto)
+        
+        return signImage
+    }()
+    
+    private let emailTextField = {
+        let textEmail = UITextField()
+        
+        textEmail.backgroundColor = UIColor(named: Constants.Texts.textEmailBackColor)
+        textEmail.text = Constants.Texts.emailText
+        textEmail.textColor = UIColor(named: Constants.Texts.textColor)
+        textEmail.font = UIFont.systemFont(ofSize: 21)
+        textEmail.borderStyle = .roundedRect
+        textEmail.layer.cornerRadius = 25
+        textEmail.layer.masksToBounds = true
+        
+        return textEmail
+    }()
+    
+    private let passwordlTextField = {
+        let textPass = UITextField()
+        
+        textPass.backgroundColor = UIColor(named: Constants.Texts.textEmailBackColor)
+        textPass.text = Constants.Texts.emailPassword
+        textPass.textColor = UIColor(named: Constants.Texts.textColor)
+        textPass.font = UIFont.systemFont(ofSize: 21)
+        textPass.borderStyle = .roundedRect
+        textPass.layer.cornerRadius = 25
+        textPass.layer.masksToBounds = true
+
+        return textPass
+    }()
+    
+    private let signUpButton = {
+        let logButton = UIButton()
+        
+        logButton.setTitle("Sign Up", for: .normal)
+        logButton.setTitleColor(UIColor(named: "buttonTitleColor"), for: .normal)
+        logButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 30)
+
+        return logButton
+    }()
+    
+    private let goSignInButton: UIButton = {
+        let signInButton = UIButton()
+        
+        signInButton.setTitle("Sign In", for: .normal)
+        signInButton.setTitleColor(UIColor(
+            named: Constants.Texts.signInButtonColor),
+            for: .normal)
+        signInButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
+        signInButton.addTarget(self, action: #selector(signInButtonTapped), for: .touchUpInside)
+        
+        return signInButton
+    }()
+    
+    private let haveLabel = {
+        let label = UILabel()
+        
+        label.text = Constants.Texts.iHaveAccaount
+        label.font = .systemFont(ofSize: 15)
+        label.textColor = .black
+        label.textAlignment = .center
+        
+        return label
+    }()
+    
+    //MARK: - Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupSubviews()
+        configureConstraints()
+    }
+    
+    //MARK: - Private Methods
+    
+    private func setupSubviews(){
+        view.backgroundColor = UIColor(named: "singnUpBackColor")
+        
+        view.addSubview(signUpLogo)
+        view.addSubview(emailTextField)
+        view.addSubview(passwordlTextField)
+        view.addSubview(signUpButton)
+        view.addSubview(goSignInButton)
+        view.addSubview(haveLabel)
+
+    }
+    
+    @objc private func signInButtonTapped() {
+        let signInViewController = SignInViewController()
+        signInViewController.modalPresentationStyle = .fullScreen
+        present(signInViewController, animated: true, completion: nil)
+    }
+    
+    private func configureConstraints() {
+        signUpLogo.translatesAutoresizingMaskIntoConstraints = false
+        emailTextField.translatesAutoresizingMaskIntoConstraints = false
+        passwordlTextField.translatesAutoresizingMaskIntoConstraints = false
+        signUpButton.translatesAutoresizingMaskIntoConstraints = false
+        goSignInButton.translatesAutoresizingMaskIntoConstraints = false
+        haveLabel.translatesAutoresizingMaskIntoConstraints = false
+
+        configureSignUpLogoConstraints()
+        configureTextFieldEmailConstraints()
+        configureTextFieldPassConstraints()
+        configureSignUpButtonConstraints()
+        configureGoSignInButtonConstraints()
+        configureHaveLabelConstraints()
+    }
+}
+
+extension SignUpViewController{
+    
+    private func configureSignUpLogoConstraints() { NSLayoutConstraint.activate([
+        signUpLogo.topAnchor.constraint(
+            equalTo: view.topAnchor,
+            constant: Constants.Insets.topSignUpLogo
+        ),
+        signUpLogo.leadingAnchor.constraint(
+            equalTo: view.leadingAnchor,
+            constant: Constants.Insets.leadingSignUpLogo
+        ),
+        signUpLogo.trailingAnchor.constraint(
+            equalTo: view.trailingAnchor,
+            constant: Constants.Insets.trailingSignUpLogo
+        ),
+        signUpLogo.heightAnchor.constraint(
+            equalToConstant: Constants.Insets.heightSignUpLogo
+        )
+     ])
+   }
+    private func configureTextFieldEmailConstraints() {
+        NSLayoutConstraint.activate([
+            emailTextField.topAnchor.constraint(
+                equalTo: signUpLogo.bottomAnchor,
+                constant: Constants.Insets.bottomtextFieldEmail
+        ),
+            emailTextField.leadingAnchor.constraint(
+                equalTo: signUpLogo.leadingAnchor,
+                constant: Constants.Insets.leadingSignUpLogo
+        ),
+            emailTextField.trailingAnchor.constraint(
+                equalTo: signUpLogo.trailingAnchor,
+                constant: Constants.Insets.trailingSignUpLogo
+        ),
+            emailTextField.heightAnchor.constraint(
+                equalToConstant: Constants.Insets.heightTextFieldEmail
+        )
+     ])
+   }
+    private func configureTextFieldPassConstraints() {
+        NSLayoutConstraint.activate([
+            passwordlTextField.topAnchor.constraint(
+                equalTo: emailTextField.bottomAnchor,
+                constant: Constants.Insets.bottomTextFieldPass
+        ),
+            passwordlTextField.leadingAnchor.constraint(
+                equalTo: signUpLogo.leadingAnchor,
+                constant: Constants.Insets.leadingSignUpLogo
+        ),
+            passwordlTextField.trailingAnchor.constraint(
+                equalTo: signUpLogo.trailingAnchor,
+                constant: Constants.Insets.trailingSignUpLogo
+        ),
+            passwordlTextField.heightAnchor.constraint(
+                equalToConstant: Constants.Insets.heightTextFieldEmail
+        )
+     ])
+   }
+    private func configureSignUpButtonConstraints() {
+        NSLayoutConstraint.activate([
+            signUpButton.topAnchor.constraint(
+                equalTo: passwordlTextField.bottomAnchor,
+                constant: Constants.Insets.topSignUpButton
+        ),
+            signUpButton.leadingAnchor.constraint(
+                equalTo: view.leadingAnchor,
+                constant: Constants.Insets.leadingSignUpButton
+        ),
+            signUpButton.trailingAnchor.constraint(
+                equalTo: view.trailingAnchor,
+                constant: Constants.Insets.trailingSignUpButton
+        ),
+            signUpButton.heightAnchor.constraint(
+                equalToConstant: Constants.Insets.heightSignUpButton
+        )
+     ])
+   }
+    private func configureGoSignInButtonConstraints() {
+        NSLayoutConstraint.activate([
+            goSignInButton.topAnchor.constraint(
+                equalTo: view.topAnchor,
+                constant: Constants.Insets.topSignInButton
+        ),
+            goSignInButton.leadingAnchor.constraint(
+                equalTo: view.leadingAnchor,
+                constant: Constants.Insets.leadingSignInButton
+        ),
+            goSignInButton.trailingAnchor.constraint(
+                equalTo: view.trailingAnchor,
+                constant: Constants.Insets.trailingSignInButton
+        ),
+            goSignInButton.bottomAnchor.constraint(
+                equalTo: view.bottomAnchor,
+                constant: Constants.Insets.bottomSignInButton
+        )
+     ])
+   }
+    private func configureHaveLabelConstraints() {
+        NSLayoutConstraint.activate([
+            haveLabel.topAnchor.constraint(
+                equalTo: view.topAnchor,
+                constant: Constants.Insets.topHaveLabel
+        ),
+            haveLabel.leadingAnchor.constraint(
+                equalTo: view.leadingAnchor,
+                constant: Constants.Insets.leadingHaveLabel
+        ),
+            haveLabel.trailingAnchor.constraint(
+                equalTo: view.trailingAnchor,
+                constant: Constants.Insets.trailingHaveLabel
+        ),
+            haveLabel.bottomAnchor.constraint(
+                equalTo: view.bottomAnchor,
+                constant: Constants.Insets.bottomHaveLabel
+        )
+     ])
+   }
+}
+
